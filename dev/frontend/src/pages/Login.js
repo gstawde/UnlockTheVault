2fbda314511ab6be75e903711c54bb4b812f8426
Gargi Tawde <gargi.tawde01@gmail.com>
2024-04-28 23:12:32 -0700

    Sign up and login form basic validation + show/hide password. Information from input tags are saved.

--- dev/frontend/src/pages/Login.js
+++ dev/frontend/src/pages/Login.js
@@ -1,8 +1,26 @@
 import './login.css';
-
+import React, { useState } from 'react';
 import UnlockLogo from '../assets/unlockLogo.png';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [showPassword, setShowPassword] = useState(false);
+    const handleTogglePassword = (e) => {
+        e.preventDefault(); // Prevent the default form submission
+        setShowPassword(!showPassword);
+    };
+
+    const handleLogin = () => {
+        if (username.trim() === '' || password.trim() === '') {
+            alert('You missed a spot! Make sure to provide both a username AND password.');
+        } else {
+            // Perform login logic here
+            console.log('Logging in...');
+        }
+    };
+
     return (
         <html lang="en">
         <head>
@@ -15,10 +33,13 @@ const Login = () => {
             <h1>Welcome Back!</h1>
             <form>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" name="username" placeholder="Username"/>
+                <input type="text" id="username" name="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
                 <label htmlFor="password">Password</label>
-                <input type="text" id="password" name="password" placeholder="Password"/>
-                <input type="submit" value="Log In"/>
+                <div className="show-pass-row">
+                    <input type={showPassword ? 'text' : 'password'} id="password" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <button className="show-pass-button" onClick={handleTogglePassword}>{showPassword ? 'Hide Password' : 'Show Password'}</button>
+                </div>
+                <button type="button" className="login" onClick={handleLogin}>Login</button>
             </form>
         </body>
         </html>
