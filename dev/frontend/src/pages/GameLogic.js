99ab5f22d6cbaa719596cd579018c4be7570bcc1
Gargi Tawde <gargi.tawde01@gmail.com>
2024-05-01 19:24:27 -0700

    User is alerted when they've won (gotten correct guesses for all 6 spots)

--- dev/frontend/src/pages/GameLogic.js
+++ dev/frontend/src/pages/GameLogic.js
@@ -34,56 +34,64 @@ const updateTestContent = (newContent, setHint) => {
     setHint(newContent);
 };
 
-export const handleButtonClick = (boxId, inputValue, backgroundColor, setBackgroundColor, setHint) => {
+export const checkSubmittedGuess = (boxId, inputValue, backgroundColor, setBackgroundColor, setHint) => {
+    let ansCorrect = false;
     if (boxId === 'one') {
         if (testArr[0] === parseInt(inputValue, 10)) {
             setBackgroundColor('#309B35'); // Green hex code
             updateTestContent('', setHint);
+            ansCorrect = true;
         } else {
             setBackgroundColor('#C72721'); // Red hex code
             updateTestContent('', setHint);
         }
     } else if (boxId === 'two') {
         if (testArr[1] === parseInt(inputValue, 10)) {
-            setBackgroundColor('#309B35'); // Red hex code
+            setBackgroundColor('#309B35');
             updateTestContent('', setHint);
+            ansCorrect = true;
         } else {
-            setBackgroundColor('#C72721'); // Red hex code
+            setBackgroundColor('#C72721');
             updateTestContent('', setHint);
         }
     } else if (boxId === 'three') {
         if (testArr[2] === parseInt(inputValue, 10)) {
-            setBackgroundColor('#309B35'); // Red hex code
+            setBackgroundColor('#309B35');
             updateTestContent('', setHint);
+            ansCorrect = true;
         } else {
             setBackgroundColor('#C72721'); // Red hex code
             updateTestContent('', setHint);
         }
     } else if (boxId === 'four') {
         if (testArr[3] === parseInt(inputValue, 10)) {
-            setBackgroundColor('#309B35'); // Red hex code
+            setBackgroundColor('#309B35');
             updateTestContent('', setHint);
+            ansCorrect = true;
         } else {
             setBackgroundColor('#C72721'); // Red hex code
             updateTestContent('', setHint);
         }
     } else if (boxId === 'five') {
         if (testArr[4] === parseInt(inputValue, 10)) {
-            setBackgroundColor('#309B35'); // Red hex code
+            setBackgroundColor('#309B35');
             updateTestContent('', setHint);
+            ansCorrect = true;
         } else {
             setBackgroundColor('#C72721'); // Red hex code
             updateTestContent('', setHint);
         }
     } else if (boxId === 'six') {
         if (testArr[5] === parseInt(inputValue, 10)) {
-            setBackgroundColor('#309B35'); // Red hex code
+            setBackgroundColor('#309B35');
             updateTestContent('', setHint);
+            ansCorrect = true;
         } else {
             setBackgroundColor('#C72721'); // Red hex code
             updateTestContent('', setHint);
         }
     }
+    return ansCorrect;
 };
 
 export const handleHintButton = (inputValue, setHint, spotNumber) => {
